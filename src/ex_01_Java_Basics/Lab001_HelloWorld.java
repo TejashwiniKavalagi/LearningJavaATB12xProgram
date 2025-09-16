39f7113ea7fe4f9ecbd904009e4f91a35094ec7d
TejashwiniKavalagi <tejukavalagi@gmail.com>
2025-09-16 17:13:34 +0530

    16 Sept 2025

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi, Tejashwini");
+
+        // This is Tejashwini, This code will not executed
+
+        /*
+         * Author :  Tejashwini
+         *
+         *
+         * */
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,32 @@
+package ex_01_Java_Basics;
+/**
+ * Author : Tejashwini
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
+
+public class Lab006_Comments_Multi {
+    /**
+     * Author : Tejashwini
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+    public static void main(String[] args) {
+        /**
+         * Author : Tejashwini
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         **/
+        System.out.println("Multi Comment");
+    }
+}
+/**
+ * Author : Tejashwini
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
+
